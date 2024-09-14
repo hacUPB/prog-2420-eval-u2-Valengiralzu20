217735dc9236c8ed756d1913d4904c890bf6ce38
Valentina Giraldo <valentina.giraldoz@upb.edu.co>
2024-09-13 22:13:35 -0500

    nuevas modificaciones en el código

--- src/satelite.py
+++ src/satelite.py
@@ -4,38 +4,38 @@ def main():
 
     import math #Para poder utilizar operaciones matemáticas 
 
-    def simulacion_desintegracion(altitud_inicial, coeficiente_arrastre_inicial, altitud_minima):
+    def simulacion_desintegracion(altitud_satelite_inicial, cd_inicial, altitud_satelite_minima):
 
-        altitud_actual = altitud_inicial
-        coeficiente_arrastre = coeficiente_arrastre_inicial
+        altitud_satelite_actual = altitud_satelite_inicial
+        cd = cd_inicial
         orbitas = 0
 
         print("\nA continuación se puede evidenciar una simulación de la desintegración orbital de un satélite:")
 
-        while altitud_actual > altitud_minima:
+        while altitud_satelite_actual > altitud_satelite_minima:
             # Calcula la pérdida de altitud
-            perdida_altitud = coeficiente_arrastre * altitud_actual
+            altitud_perdida = cd * altitud_satelite_actual
 
             # Actualiza la altitud
-            altitud_actual=altitud_actual-perdida_altitud
+            altitud_satelite_actual=altitud_satelite_actual-altitud_perdida 
 
             # Aumenta el coeficiente de arrastre (simplificación)
-            coeficiente_arrastre = coeficiente_arrastre +0.0001
+            cd = cd + 0.0001
 
             # Incrementa el contador de órbitas
             orbitas = orbitas + 1
 
-            print(f"Órbita {orbitas}: Altitud actual = {altitud_actual:.2f} km, Coeficiente de arrastre = {coeficiente_arrastre:.4f}")
+            print(f"Órbita {orbitas}: Altitud actual = {altitud_satelite_actual} km, Coeficiente de arrastre = {cd}")
 
         print(f"\nEl satélite ha reingresado en la atmósfera terrestre después de {orbitas} órbitas.")
 
     # Obtener datos del usuario
-    altitud_inicial = float(input("\nIngrese la altitud inicial del satélite (en kilómetros): "))
-    coeficiente_arrastre_inicial = float(input("\nIngrese el coeficiente de arrastre inicial: "))
+    altitud_satelite_inicial = float(input("\nIngrese la altitud inicial del satélite (en kilómetros): "))
+    cd_inicial = float(input("\nIngrese el coeficiente de arrastre inicial: "))
     altitud_minima = float(input("\nIngrese la altitud mínima segura (en kilómetros): "))
 
     # Iniciar la simulación
-    simulacion_desintegracion(altitud_inicial, coeficiente_arrastre_inicial, altitud_minima)
+    simulacion_desintegracion(altitud_satelite_inicial, cd_inicial, altitud_minima)
 
 
 if __name__ == "__main__":
